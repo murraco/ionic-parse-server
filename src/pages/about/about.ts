4e363c183b9f188f876d5920c3a45adbc47ef27d
murraco <mauriurraco@gmail.com>
2019-05-25 22:26:11 -0300

    Update dependencies

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -13,7 +13,7 @@ import { SigninPage } from '../signin/signin';
 })
 export class AboutPage {
 
-  constructor(private auth: AuthProvider, private navCtrl: NavController, private app: App) { }
+  constructor(private auth: AuthProvider, private app: App) { }
 
   ionViewCanEnter(): boolean {
     return this.auth.authenticated();
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { App, NavController } from 'ionic-angular';
+import { App } from 'ionic-angular';
 
 // Providers
 import { ParseProvider } from '../../providers/parse/parse';
@@ -16,7 +16,7 @@ export class HomePage {
   newScore = { playerName: null, score: null };
   gameScores = [];
 
-  constructor(private parseProvider: ParseProvider, private auth: AuthProvider, private navCtrl: NavController, private app: App) {
+  constructor(private parseProvider: ParseProvider, private auth: AuthProvider, private app: App) {
     this.listScores();
   }
 
